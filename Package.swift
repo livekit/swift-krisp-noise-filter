bd4b607be0800d8ab3c13e12e775b0296df93416
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2024-06-04 18:13:19 +0900

    v0.0.1

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .binaryTarget(
             name: "KrispNoiseFilter",
             url: "https://github.com/livekit/swift-krisp-noise-filter/releases/download/0.0.1/KrispNoiseFilter.xcframework.zip",
-            checksum: "38ab9f625f85157bb67477fff53ea0cc7a02742d121b356adf6b6d8bc0165d22"
+            checksum: "fecd3f9b246cd3abc971e94edb45e643cc282bbfe5136278b704a3b8aea4e8f1"
         ),
         .target(
             name: "LiveKitKrispNoiseFilter",
